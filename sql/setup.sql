a7497bda8d499728db31c842d1c32f541cf21a9f
Elise Muellerleile <eliamue@live.com>
2022-03-12 15:32:02 -0600

    SQL table added

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS companies CASCADE;
+
+CREATE TABLE companies (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    website TEXT NOT NULL,
+    logo TEXT NOT NULL,
+    quote TEXT,
+    death_coverage BOOLEAN,
+    cured_condition_eligible TEXT,
+    signup_fee TEXT,
+    deductable TEXT,
+    unique TEXT,
+    reviews TEXT,
+    cancellation TEXT,
+    reimbursement_max TEXT,
+    reimbursement_rate TEXT,
+    dental BOOLEAN,
+    food BOOLEAN
+)
