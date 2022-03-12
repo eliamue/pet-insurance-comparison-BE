31545044a2c8016ac91073c88a9c3f4277c5f94c
Elise Muellerleile <eliamue@live.com>
2022-03-12 15:36:29 -0600

    create company test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,31 @@ describe('pet-insurance-comparison-BE routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('creates a new company', async () => {
+    const company = {
+      name: 'Fetch (formerly Pet Plan)',
+      cured_condition_eligible: 'available after 12 months',
+      dental: true,
+      food: true,
+      unique: 'E-vet visits',
+      death_coverage: false,
+      deductable: '$250',
+      cancellation: '30-day money-back guarantee',
+      reviews: 'good',
+      reimbursement_rate: '90%',
+      reimbursement_max: '$5000',
+      quote: '$45',
+      signup_fee: 'No',
+      website: 'https://www.fetchpet.com/',
+      logo: 'https://www.petinsurancequotes.com/app/uploads/2022/01/fetch-pet-insurance.png',
+    };
+
+    const res = await request(app).post('/api/v1/companies').send(company);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...company,
+    });
+  });
 });
